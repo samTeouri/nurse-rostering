f9fe096f40fc4b951e4b09d91ff9a064b04d783f
Samrou Teouri <teourisamrou@gmail.com>
2024-12-02 02:51:51 +0100

    Constraint 5

--- solver.py
+++ solver.py
@@ -53,3 +53,18 @@ def solve_nurse_rostering(data):
         model.add_constraint(total_hours >= min_hours, f"min_hours_{e}")
         model.add_constraint(total_hours <= max_hours, f"max_hours_{e}")
 
+
+    # 5. Contraintes sur les jours de travail consécutifs
+    for e in staff:
+        min_consec = int(staff[e]["constraints"][4])
+        max_consec = int(staff[e]["constraints"][3])
+        for d in range(horizon - min_consec + 1):
+            model.add_constraint(
+                model.sum(x[e, d + k, s] for k in range(min_consec) for s in shifts) >= min_consec,
+                f"min_consec_{e}_{d}"
+            )
+        for d in range(horizon - max_consec + 1):
+            model.add_constraint(
+                model.sum(x[e, d + k, s] for k in range(max_consec) for s in shifts) <= max_consec,
+                f"max_consec_{e}_{d}"
+            )
