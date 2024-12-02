582849e7d519be950b73468e4e06eca7e684d838
Samrou Teouri <teourisamrou@gmail.com>
2024-12-02 02:46:46 +0100

    Model creation and data loading

---
+++ solver.py
@@ -0,0 +1,15 @@
+from docplex.mp.model import Model
+
+def solve_nurse_rostering(data):
+
+    # Création du modèle
+    model = Model(name="Nurse Rostering")
+
+    # Chargement des données depuis `data`
+    horizon = data["horizon"]
+    shifts = data["shifts"]
+    staff = data["staff"]
+    days_off = data["days_off"]
+    shift_on_requests = data["shift_on_requests"]
+    shift_off_requests = data["shift_off_requests"]
+    cover = data["cover"]
