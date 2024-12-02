fbba21fbe61c23c5a3be47ae80d1cd84673526d7
Samrou Teouri <teourisamrou@gmail.com>
2024-12-02 02:47:17 +0100

    Adding decision variables

--- solver.py
+++ solver.py
@@ -13,3 +13,17 @@ def solve_nurse_rostering(data):
     shift_on_requests = data["shift_on_requests"]
     shift_off_requests = data["shift_off_requests"]
     cover = data["cover"]
+
+    # Variables de décision
+    x = model.binary_var_dict(
+        ((e, d, s) for e in staff for d in range(horizon) for s in shifts),
+        name="x"
+    )
+    y_min = model.integer_var_dict(
+        ((d, s) for d in range(horizon) for s in shifts),
+        name="y_min"
+    )
+    y_max = model.integer_var_dict(
+        ((d, s) for d in range(horizon) for s in shifts),
+        name="y_max"
+    )
